502efef7ef103a1a49aa4000199ba6d9381a5abf
Toheed Jahan Khan <tkhan6@sapient.com>
2020-03-11 22:10:09 +0530

    High order Function Demo

---
+++ HighOrderFunctionDemo/test/HighOrderFunctionDemo_test.dart
@@ -0,0 +1,8 @@
+import 'package:HighOrderFunctionDemo/HighOrderFunctionDemo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
